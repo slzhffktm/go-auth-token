ea9006f160874eca8d9f503424eb47ba6c4ed667
slzhffktm <nic.nicholas1907@gmail.com>
2023-04-20 12:18:33 +0700

    feat: support asymmetric signing method

--- auth_token.go
+++ auth_token.go
@@ -18,7 +18,7 @@ type Options struct {
 	SigningMethod SigningMethod
 
 	// SecretKey is required for symmetric SigningMethod.
-	SecretKey string
+	SecretKey []byte
 
 	// PrivateKey is required for asymmetric SigningMethod.
 	PrivateKey any
@@ -33,11 +33,10 @@ type AuthToken[T any] struct {
 // New generates a new AuthToken instance.
 // T is the data type that wants to be inserted into the token.
 func New[T any](opts Options) (*AuthToken[T], error) {
-	if isSigningMethodSymmetric(opts.SigningMethod) {
-		if opts.SecretKey == "" {
-			return nil, errors.New("SecretKey is required for symmetric SigningMethod.")
-		}
-	} else {
+	if isSigningMethodSymmetric(opts.SigningMethod) && len(opts.SecretKey) == 0 {
+		return nil, errors.New("SecretKey is required for symmetric SigningMethod.")
+
+	} else if !isSigningMethodSymmetric(opts.SigningMethod) {
 		if opts.PrivateKey == "" || opts.PublicKey == "" {
 			return nil, errors.New("PrivateKey and PublicKey is required for asymmetric SigningMethod.")
 		}
@@ -63,6 +62,8 @@ func validatePrivatePublicKey(opts Options) error {
 		if !ok {
 			return ErrInvalidKeyType
 		}
+
+		return nil
 	case SigningMethodES256, SigningMethodES384, SigningMethodES512:
 		_, ok := opts.PrivateKey.(*ecdsa.PrivateKey)
 		if !ok {
@@ -72,6 +73,8 @@ func validatePrivatePublicKey(opts Options) error {
 		if !ok {
 			return ErrInvalidKeyType
 		}
+
+		return nil
 	case SigningMethodRS256, SigningMethodRS384, SigningMethodRS512:
 		_, ok := opts.PrivateKey.(*rsa.PrivateKey)
 		if !ok {
@@ -81,9 +84,11 @@ func validatePrivatePublicKey(opts Options) error {
 		if !ok {
 			return ErrInvalidKeyType
 		}
+
+		return nil
+	default:
+		return ErrInvalidSigningMethod
 	}
-	
-	return ErrInvalidSigningMethod
 }
 
 // GenerateToken generates a token with its expiry date.
@@ -104,7 +109,8 @@ func (a AuthToken[T]) GenerateToken(
 			IssuedAt:  jwt.NewNumericDate(time.Now()),
 		},
 	})
-	tokenString, err := unsigned.SignedString([]byte(a.opts.SecretKey))
+
+	tokenString, err := unsigned.SignedString(a.getSigningKey())
 	if err != nil {
 		return "", time.Time{}, fmt.Errorf("token.SignedString: %w", err)
 	}
@@ -125,7 +131,7 @@ func (a AuthToken[T]) ParseToken(
 		if token.Method != a.opts.SigningMethod {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
-		return []byte(a.opts.SecretKey), nil
+		return a.getParsingKey(), nil
 	})
 	if err != nil {
 		return nil, fmt.Errorf("jwt.ParseWithClaims: %w", err)
@@ -135,4 +141,20 @@ func (a AuthToken[T]) ParseToken(
 	}
 
 	return &claims.Data, nil
+}
+
+func (a AuthToken[T]) getSigningKey() any {
+	if isSigningMethodSymmetric(a.opts.SigningMethod) {
+		return a.opts.SecretKey
+	} else {
+		return a.opts.PrivateKey
+	}
+}
+
+func (a AuthToken[T]) getParsingKey() any {
+	if isSigningMethodSymmetric(a.opts.SigningMethod) {
+		return a.opts.SecretKey
+	} else {
+		return a.opts.PublicKey
+	}
 }
--- auth_token_test.go
+++ auth_token_test.go
@@ -1,6 +1,11 @@
 package authtoken_test
 
 import (
+	"crypto/ecdsa"
+	"crypto/ed25519"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/rsa"
 	"testing"
 	"time"
 
@@ -12,17 +17,18 @@ type Data struct {
 	Email string `json:"email"`
 }
 
-var secretKey = "secretKey"
+var data = Data{
+	Email: "test@osterone.com",
+}
 
-func TestAuthToken_GenerateAndParseToken_SigningMethodHS(t *testing.T) {
+func TestAuthToken_GenerateAndParseToken_SigningMethodHS256(t *testing.T) {
+	secretKey := "steve-ao-key"
+
 	authToken, err := authtoken.New[Data](authtoken.Options{
-		SecretKey:     secretKey,
+		SecretKey:     []byte(secretKey),
 		SigningMethod: authtoken.SigningMethodHS256,
 	})
-
-	data := Data{
-		Email: "test@osterone.com",
-	}
+	assert.NoError(t, err)
 
 	token, expiresAt, err := authToken.GenerateToken(data, time.Minute*5)
 	assert.NoError(t, err)
@@ -36,15 +42,62 @@ func TestAuthToken_GenerateAndParseToken_SigningMethodHS(t *testing.T) {
 	assert.Equal(t, data, *resData)
 }
 
-func TestAuthToken_GenerateAndParseToken_SigningMethodHS(t *testing.T) {
-	authToken, err := authtoken.New[Data](authtoken.Options{
-		SecretKey:     secretKey,
-		SigningMethod: authtoken.SigningMethodHS256,
-	})
+func TestAuthToken_GenerateAndParseToken_SigningMethodEdDSA(t *testing.T) {
+	pub, priv, err := ed25519.GenerateKey(rand.Reader)
+	assert.NoError(t, err)
 
-	data := Data{
-		Email: "test@osterone.com",
-	}
+	authToken, err := authtoken.New[Data](authtoken.Options{
+		SigningMethod: authtoken.SigningMethodEdDSA,
+		PrivateKey:    priv,
+		PublicKey:     pub,
+	})
+	assert.NoError(t, err)
+
+	token, expiresAt, err := authToken.GenerateToken(data, time.Minute*5)
+	assert.NoError(t, err)
+
+	assert.NotEmpty(t, token)
+	assert.NotEmpty(t, expiresAt)
+
+	resData, err := authToken.ParseToken(token)
+	assert.NoError(t, err)
+
+	assert.Equal(t, data, *resData)
+}
+
+func TestAuthToken_GenerateAndParseToken_SigningMethodES384(t *testing.T) {
+	priv, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	assert.NoError(t, err)
+
+	authToken, err := authtoken.New[Data](authtoken.Options{
+		SigningMethod: authtoken.SigningMethodES384,
+		PrivateKey:    priv,
+		PublicKey:     priv.Public(),
+	})
+	assert.NoError(t, err)
+
+	token, expiresAt, err := authToken.GenerateToken(data, time.Minute*5)
+	assert.NoError(t, err)
+
+	assert.NotEmpty(t, token)
+	assert.NotEmpty(t, expiresAt)
+
+	resData, err := authToken.ParseToken(token)
+	assert.NoError(t, err)
+
+	assert.Equal(t, data, *resData)
+}
+
+func TestAuthToken_GenerateAndParseToken_SigningMethodRS512(t *testing.T) {
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	assert.NoError(t, err)
+
+	authToken, err := authtoken.New[Data](authtoken.Options{
+		SigningMethod: authtoken.SigningMethodRS512,
+		PrivateKey:    priv,
+		PublicKey:     priv.Public(),
+	})
+	assert.NoError(t, err)
 
 	token, expiresAt, err := authToken.GenerateToken(data, time.Minute*5)
 	assert.NoError(t, err)
